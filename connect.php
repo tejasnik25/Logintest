240afc39413a52cb03a14fe2099df53507c79620
tejasnikam25 <tejas250502@gmail.com>
2024-11-06 12:57:56 +0530

    First commit

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$host="localhost";
-$user="root";
-$pass="";
-$db="login";
+$host="testdb.cp6e46ke6f6d.us-east-1.rds.amazonaws.com";
+$user="admin";
+$pass="Test-db24#";
+$db="testdb";
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
     echo "Failed to connect DB".$conn->connect_error;
